9b9be3392001eb78bbefc89f5567ae94c32f7165
MarmoW <115847012+MarmoW@users.noreply.github.com>
2022-11-20 03:36:37 -0300

    Arrumando coisas e organizando

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,33 @@
-import ReactDOM from "react-dom";
-import Navbar from "./navbar.js"
+import Navbar from "./Navbar.js";
+import Stories from "./Stories.js";
+import Posts from "./Posts.js";
+import Usuario from "./Usuario.js";
+import Sugestoes from "./Sugestoes.js";
 
-function App() {
+
+export default function App() {
     return(
         <>
             <Navbar />
+            <div class="corpo">
+                <div class="esquerda">
+                    <Stories />
+                    <Posts />
+                </div>
+                <div class="sidebar">
+                    <Usuario />
+                    <Sugestoes />
+                </div>
+            </div>
+            <div class="fundo-mobile">
+                <ion-icon name="home"></ion-icon>
+                <ion-icon name="search-outline"></ion-icon>
+                <ion-icon name="add-circle-outline"></ion-icon>
+                <ion-icon name="heart-outline"></ion-icon>
+                <ion-icon name="person-outline"></ion-icon>
+            </div>
         </>
     )
 }
 
-ReactDOM.render(<App />, document.querySelector(".root"));
+//ReactDOM.render(App, document.querySelector(".root"));
