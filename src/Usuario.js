c6e24f464960b3f91eeff30b0c3342509028cdd1
MarmoW <115847012+MarmoW@users.noreply.github.com>
2022-11-21 04:54:04 -0300

    Talvez quebre tudo

--- src/Usuario.js
+++ src/Usuario.js
@@ -1,24 +1,41 @@
-let Username = [{user:"catanacomics", nome:"Catana",pfp:"assets/img/catanacomics.svg"}]
+import React from "react";
 
 export default function Usuario(){
-    return(
+  const [nome, setNome] = React.useState("Catana");
+  const [pfp, setPfp] = React.useState("assets/img/catanacomics.svg");
+  let Username = [{user:"catanacomics", nome:{nome} ,pfp:{pfp}}]
+
+  function MudarFoto(){
+    let novaPfp = prompt("Digite o link da sua nova foto de perfil.");
+    novaPfp ? setPfp(novaPfp) : null;
+    
+  }
+  function MudarNome(){
+    let novoNome = prompt("Para qual nome gostaria de mudar?");
+    novoNome ? setNome(novoNome) : null;
+  }
+  function User(props) {
+    return (
+      <>
+        <img onClick={MudarFoto} src={pfp ? pfp : "assets/img/catanacomics.svg"}/>
+        <div className="texto">
+              <strong>{props.user}</strong>
+              <span>
+              {nome ? nome : "Catana"}
+              <ion-icon name="pencil" onClick={MudarNome}></ion-icon>
+          </span>
+        </div>
+      </>
+  
+    );
+  }
+  return(
         <div className="usuario">
           {Username.map((userinfo) => <User user={userinfo.user} nome={userinfo.nome} pfp={userinfo.pfp}/>)}
         </div>
-    );
-}
-function User(props) {
-  return (
-    <>
-      <img src={props.pfp} />
-      <div className="texto">
-            <strong>{props.user}</strong>
-            <span>
-            {props.nome}
-            <ion-icon name="pencil"></ion-icon>
-        </span>
-      </div>
-    </>
-
   );
 }
+
+
+
+//
